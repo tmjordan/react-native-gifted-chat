09ee3affe2273998661a6573b994a37033bfb51e
Xavier Carpentier <xcapetir@gmail.com>
2019-05-01 17:47:15 +0200

    feat add quick replies

--- src/GiftedChat.tsx
+++ src/GiftedChat.tsx
@@ -30,7 +30,7 @@ import {
   TIME_FORMAT,
   DATE_FORMAT,
 } from './Constant'
-import { IMessage, User } from './types'
+import { IMessage, User, Reply } from './types'
 
 const GiftedActionSheet = ActionSheet as any
 
@@ -146,6 +146,7 @@ export interface GiftedChatProps<TMessage extends IMessage = IMessage> {
   onInputTextChanged?(text: string): void
   /* Custom parse patterns for react-native-parsed-text used to linking message content (like URLs and phone numbers) */
   parsePatterns?(): React.ReactNode
+  onQuickReply?(reply: Reply): void
 }
 
 interface GiftedChatState {
